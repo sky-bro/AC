9dd82184290f677831ccd00962fdca613bb59bce
sky-bro <sky_bro@qq.com>
2019-11-14 21:25:23 +0800

    code review, add more comments, minor bug fix

--- Algorithms/LCS/main.cpp
+++ Algorithms/LCS/main.cpp
@@ -22,6 +22,7 @@ int LCS(const char *str1, int len1, const char *str2, int len2)
     if (str1[len1 - 1] == str2[len2 - 1])
     {
         dp[len1 - 1][len2 - 1] = LCS(str1, len1 - 1, str2, len2 - 1);
+        // 3表示左上
         result[len1][len2] = 3;
         return 1 + dp[len1 - 1][len2 - 1];
     }
@@ -33,19 +34,19 @@ int LCS(const char *str1, int len1, const char *str2, int len2)
         dp[len1][len2 - 1] = seq_len2;
         if (seq_len1 > seq_len2)
         {
-            result[len1][len2] = 1;
+            result[len1][len2] = 1; // 1表示上
             return seq_len1;
         }
         else
         {
-            result[len1][len2] = 2;
+            result[len1][len2] = 2; // 2表示左
             return seq_len2;
         }
     }
 }
 
 // 递推
-void LCS2(const char *str1, int len1, const char *str2, int len2)
+int LCS2(const char *str1, int len1, const char *str2, int len2)
 {
     for (int i = 1; i <= len1; ++i)
     {
@@ -54,24 +55,28 @@ void LCS2(const char *str1, int len1, const char *str2, int len2)
             if (str1[i - 1] == str2[j - 1])
             {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
+                // 3表示左上
                 result[i][j] = 3;
             }
-            else if (result[i - 1][j] > result[i][j - 1])
+            else if (dp[i - 1][j] > dp[i][j - 1])
             {
                 dp[i][j] = dp[i - 1][j];
+                // 1表示上，上面的dp值更长
                 result[i][j] = 1;
             }
             else
             {
                 dp[i][j] = dp[i][j - 1];
+                // 2表示左
                 result[i][j] = 2;
             }
         }
     }
+    return dp[len1][len2];
 }
 
+// 
 void print_sequence(int len1, int len2)
-
 {
     if (len1 == 0 || len2 == 0)
         return;
@@ -90,16 +95,21 @@ void print_sequence(int len1, int len2)
     }
 }
 
+// 重置动态规划用的二维数组，数组len1+1行，len2+1列
 void clear(int len1, int len2)
 {
+    // 第一行与第一列都是0，分别表示:
+    // str1的前0个字符与str2的前0~len2字符的LCS都为0
+    // str2的前0个字符与str1的前0~len1字符的LCS都为0
     for (int i = 0; i < len1; ++i)
     {
-        dp[i][0] = 0;
+        dp[i][0] = 0; // 第一列都是0
     }
     for (int j = 0; j < len1; ++j)
     {
-        dp[0][j] = 0;
+        dp[0][j] = 0; // 第一行都是0
     }
+    // 其余位置初始化为-1，表示还未设置
     for (int i = 1; i <= len1; ++i)
     {
         for (int j = 1; j <= len2; ++j)
@@ -127,14 +137,15 @@ int main(int argc, const char **argv)
     int len1 = strlen(str1);
     int len2 = strlen(str2);
 
+    // 递归求
     clear(len1, len2);
     cout << LCS(str1, len1, str2, len2) << endl;
     print_sequence(len1, len2);
     cout << endl;
 
+    // 非递归求
     clear(len1, len2);
-    LCS2(str1, len1, str2, len2);
-    cout << dp[len1][len2] << endl;
+    cout << LCS2(str1, len1, str2, len2) << endl;
     print_sequence(len1, len2);
     cout << endl;
 
