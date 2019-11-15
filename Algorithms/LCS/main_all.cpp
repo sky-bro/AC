8af90fa3de8ca10f89f074bd97b4df7d616855ec
sky-bro <sky_bro@qq.com>
2019-11-15 09:17:31 +0800

    add get_all_sequence() for all LCS

--- Algorithms/LCS/main_all.cpp
+++ Algorithms/LCS/main_all.cpp
@@ -1,5 +1,9 @@
+// 需要输出所有最长公共子序列，只需要改变print_sequence函数
 #include <iostream>
 #include <cstring>
+#include <string>
+#include <set>
+
 using namespace std;
 
 const int N = 100;
@@ -95,6 +99,42 @@ void print_sequence(int len1, int len2)
     }
 }
 
+// 
+set<string> get_all_sequence(int len1, int len2)
+{
+    set<string> s;
+    char buf[2] = {0};
+    if (len1 == 0 || len2 == 0)
+        return set<string>();
+    
+    if (dp[len1][len2] == dp[len1][len2-1]) {
+        set<string> tmp = get_all_sequence(len1, len2-1);
+        s.insert(tmp.begin(), tmp.end());
+    }
+
+    if (dp[len1][len2] == dp[len1-1][len2]) {
+        set<string> tmp = get_all_sequence(len1-1, len2);
+        s.insert(tmp.begin(), tmp.end());
+    }
+
+    if (dp[len1][len2] == dp[len1-1][len2-1]) {
+        set<string> tmp = get_all_sequence(len1-1, len2-1);
+        s.insert(tmp.begin(), tmp.end());
+    } else if (dp[len1][len2] == dp[len1-1][len2-1] + 1) {
+        set<string> tmp = get_all_sequence(len1-1, len2-1);
+        buf[0] = str1[len1-1];
+        string ss(buf);
+        if (tmp.empty()) {
+            s.insert(ss);
+        } else {
+            for (auto x: tmp) {
+                s.insert(ss + x);
+            }
+        }
+    }
+    return s;
+}
+
 // 重置动态规划用的二维数组，数组len1+1行，len2+1列
 void clear(int len1, int len2)
 {
@@ -140,13 +180,21 @@ int main(int argc, const char **argv)
     // 递归求
     clear(len1, len2);
     cout << LCS(str1, len1, str2, len2) << endl;
-    print_sequence(len1, len2);
+    set<string> s = get_all_sequence(len1, len2);
+    cout<<"size: "<<s.size()<<endl;
+    for (auto x: s){
+        cout<<x<<endl;
+    }
     cout << endl;
 
     // 非递归求
     clear(len1, len2);
     cout << LCS2(str1, len1, str2, len2) << endl;
-    print_sequence(len1, len2);
+    s = get_all_sequence(len1, len2);
+    cout<<"size: "<<s.size()<<endl;
+    for (auto x: s){
+        cout<<x<<endl;
+    }
     cout << endl;
 
     return 0;
