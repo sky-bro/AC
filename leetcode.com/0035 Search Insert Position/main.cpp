57ea5ce6f60ade0a1fb2c4ed3edb89031130a69a
sky-bro <sky_bro@qq.com>
2019-12-05 16:19:53 +0800

    lower_bound

---
+++ leetcode.com/0035 Search Insert Position/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
