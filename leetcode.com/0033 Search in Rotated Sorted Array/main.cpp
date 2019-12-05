9a5992c9a7538d8b2f1d291c0ac792c7c787c4e1
sky-bro <sky_bro@qq.com>
2019-12-05 15:23:43 +0800

    0033 Search in Rotated Sorted Array

---
+++ leetcode.com/0033 Search in Rotated Sorted Array/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0, r=nums.size()-1, m;
+        while (l <= r) {
+            m = l + (r-l)/2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] < target) { // need bigger nums[m]
+                // three cases:
+                if (nums[m] < nums[l]) { // 1. pivot point on the left
+                    if (target <= nums[r]) { // target in right half
+                        l = m + 1;
+                    } else { // target in left half
+                        r = m - 1;
+                    }
+                } else if (nums[m] > nums[r]) { // 2. pivot point on the right
+                    l = m + 1;
+                    r = r - 1;
+                } else { // 3. normal
+                    l = m + 1;
+                }
+            } else {  // need smaller nums[m]
+                 // three cases:
+                if (nums[m] < nums[l]) { // 1. pivot point on the left
+                    l = l + 1;
+                    r = m - 1;
+                } else if (nums[m] > nums[r]) { // 2. pivot point on the right
+                    if (target <= nums[r]) {
+                        l = m + 1;
+                    } else {
+                        r = m - 1;
+                    }
+                } else { // 3. normal
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
