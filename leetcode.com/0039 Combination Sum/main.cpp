88c33e623878790b444d41285aa182ccff147adb
sky-bro <sky_bro@qq.com>
2019-12-06 20:58:33 +0800

    0039 Combination Sum correct, optimize tomorrow

---
+++ leetcode.com/0039 Combination Sum/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<int>> _combinationSum(vector<int> &candidates, int target, int start_idx) {
+        vector<vector<int>> result;
+        int n = candidates.size();
+        for (int i = start_idx; i < n && candidates[i] <= target; ++i) {
+            vector<int> vi;
+            if (candidates[i] == target) {
+                vi.push_back(target);
+                result.push_back(vi);
+                continue;
+            }
+            auto prev_results = _combinationSum(candidates, target - candidates[i], i);
+            for (auto prev_result: prev_results) {
+                prev_result.insert(prev_result.begin(), candidates[i]);
+                result.push_back(prev_result);
+            }
+        }
+        return result;        
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        return _combinationSum(candidates, target, 0);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
