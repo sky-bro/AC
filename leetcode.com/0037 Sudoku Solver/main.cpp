2299190a20919ade43efc5cb0ec4ec867b7c45ef
sky-bro <sky_bro@qq.com>
2019-12-06 14:50:10 +0800

    0037 Sudoku Solver fixed, dfs

--- leetcode.com/0037 Sudoku Solver/main.cpp
+++ leetcode.com/0037 Sudoku Solver/main.cpp
@@ -6,41 +6,44 @@ using namespace std;
 class Solution {
 public:
     void solveSudoku(vector<vector<char>>& board) {
-        // brute force
-        int cnt=0;
         bool row[9][9] = { false }, col[9][9] = { false }, box[9][9] = { false };
-
         for (int r = 0; r < 9; ++r) {
             for (int c = 0; c < 9; ++c) {
                 if (board[r][c] == '.') continue;
-                cnt++;
-                int val_ind = board[r][c] - '1';
-                int box_ind = r / 3 * 3 + c / 3;
-                row[r][val_ind] = col[c][val_ind] = box[box_ind][val_ind] = true;
+                int num = board[r][c] - '1';
+                row[r][num] = col[c][num] = box[r/3*3+c/3][num] = true;
             }
         }
-        
-        while (true) {
-            for (int r = 0; r < 9; ++r) {
-                for (int c = 0; c < 9; ++c) {
-                    if (board[r][c] != '.') continue;
-                    int solved = -1, i = 0;
-                    for (; i < 9; ++i) {
-                        if (col[c][i] || row[r][i] || box[r/3*3+c/3][i]) continue;
-                        if (solved != -1) {
-                            break;
-                        } else {
-                            solved = i;
-                        }
-                    }
-                    if (solved != -1 && i == 9) {
-                        board[r][c] = solved + '1';
-                        col[c][solved] = row[r][solved] = box[r/3*3+c/3][solved] = true;
-                        if (++cnt == 81) return;
-                    }
-                }
+        solveSudoku(board, 0, row, col, box);
+    }
+private:
+    bool solveSudoku(vector<vector<char>>& board, int pos, bool row[9][9], bool col[9][9], bool box[9][9]) {
+        // goal is: when I reach the end, stop
+        if(pos == 81) {
+            return true;
+        }
+        int r = pos/9, c = pos%9;
+        // choice 1: for 1~9, do nothing, check the next one
+        if(board[r][c] != '.') {
+            return solveSudoku(board, pos+1, row, col, box);
+        }
+        // choice 2: for '.', try all possible candidates
+        for(int i = 0; i < 9; ++i) {
+            // constrain: same row/col/block can't have duplicate elements
+            // before put any candidate, check whether it is a valid solution
+            if (row[r][i] || col[c][i] || box[r/3*3+c/3][i]) {
+                continue;
+            }
+            board[r][c] = '1' + i;
+            row[r][i] = col[c][i] = box[r/3*3+c/3][i] = true;
+            if (solveSudoku(board, pos+1, row, col, box)) {
+                return true;
+            } else {
+                row[r][i] = col[c][i] = box[r/3*3+c/3][i] = false;
             }
         }
+        board[r][c] = '.';
+        return false;
     }
 };
 
