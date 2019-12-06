7f8a7812353aa60ef370bf12c3ad2884fa00d516
sky-bro <sky_bro@qq.com>
2019-12-06 23:11:38 +0800

    0041 First Missing Positive reuse nums array, save space, useless...

--- leetcode.com/0041 First Missing Positive/main.cpp
+++ leetcode.com/0041 First Missing Positive/main.cpp
@@ -9,14 +9,34 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size(), i;
-        vector<bool> _nums(n, false);
-        for (auto x: nums) {
-            if (x <= n && x > 0) _nums[x-1] = true;
-        }
+        vector<bool> _nums(n);
+        for (auto x: nums) if (x <= n && x > 0) _nums[x-1] = true;
         for (i = 0; i < n && _nums[i]; ++i);
         return i+1;
     }
 };
+
+// time: O(n), space: O(1), does this counts as O(1) space, I his this implementation is bad
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int r = nums.size(), i;
+//         for (i = 0; i < r;) {
+//             if (nums[i] > r || nums[i] <= 0) swap(nums[i], nums[--r]);
+//             else {
+//                 int ind = nums[i]-1;
+//                 if (nums[ind] == nums[i]) {
+//                     if (i == ind) ++i;
+//                     else nums[i++] = 0;
+//                 } else {
+//                     swap(nums[ind], nums[i]);
+//                 }
+//             }
+//         }
+//         for (i = 0; i < r && nums[i] == i + 1; ++i);
+//         return i+1;
+//     }
+// };
 
 // time: O(nlogn)
 // class Solution {
@@ -31,3 +51,11 @@ public:
 //         return j;
 //     }
 // };
+
+int main(int argc, char const *argv[])
+{
+    Solution s;
+    vector<int> vi = {1, 1};
+    cout<<s.firstMissingPositive(vi)<<endl;
+    return 0;
+}
