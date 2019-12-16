1060954c4dd43a391b48631cdde008327b9e9187
sky-bro <sky_bro@qq.com>
2019-12-16 09:47:07 +0800

    0064 Minimum Path Sum

---
+++ leetcode.com/0064 Minimum Path Sum/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for (int i = n - 2; i >= 0; --i) {
+            grid[m-1][i] += grid[m-1][i+1];
+        }
+        for (int i = m - 2; i >= 0; --i) {
+            grid[i][n-1] += grid[i+1][n-1];
+            for (int j = n - 2; j >= 0; --j) {
+                grid[i][j] += min(grid[i+1][j], grid[i][j+1]);
+            }
+        }
+        return grid[0][0];
+    }
+};
