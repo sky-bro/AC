4398886f9f4be90c1c407fcfcf46b7331bf9b972
sky-bro <sky_bro@qq.com>
2019-12-31 21:46:27 +0800

    0090 Subsets II: needs improve

---
+++ leetcode.com/0090 Subsets II/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<vector<int>> result;
+    size_t n;
+    void _subsetsWithDup(vector<int> &nums, int idx, vector<int> path) {
+        if (idx == n) {
+            result.push_back(path);
+            return;
+        }
+        int m = 1;
+        for (;idx + m != n && nums[idx+m] == nums[idx]; ++m);
+        _subsetsWithDup(nums, idx+m, path);
+        for (int i = 1; i <= m; ++i) {
+            path.push_back(nums[idx]);
+            _subsetsWithDup(nums, idx+m, path);
+        }
+    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        result.clear();
+        n = nums.size();
+        vector<int> path;
+        _subsetsWithDup(nums, 0, path);
+        return result;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1, 2, 2};
+    Solution s;
+    s.subsetsWithDup(nums);
+    return 0;
+}
