0810f33787e172c1517b38aef6e4e2d154857736
sky-bro <sky_bro@qq.com>
2020-01-01 19:01:23 +0800

    0096 Unique Binary Search Trees

---
+++ leetcode.com/0096 Unique Binary Search Trees/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+};
