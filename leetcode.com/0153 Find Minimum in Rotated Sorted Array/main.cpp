8c137f9c4c653d57d2a5e1fce01a80b8cb4d7dc3
sky-bro <sky_bro@qq.com>
2020-01-19 19:09:32 +0800

    0153 Find Minimum in Rotated Sorted Array: no duplicate nums, simpler

---
+++ leetcode.com/0153 Find Minimum in Rotated Sorted Array/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// no duplicate numbers, simpler
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+
+        int l = 0, r = nums.size() - 1;
+        // if (r < 0) return 0; // 不考虑
+        int result = nums[0];
+        while (l <= r) {
+            int mid = l + (r - l)/2;
+            if (nums[mid] < result) result = nums[mid];
+            if (nums[mid] < nums[r]) r = mid;
+            else l = mid + 1;
+        }
+        return result;
+    }
+};
