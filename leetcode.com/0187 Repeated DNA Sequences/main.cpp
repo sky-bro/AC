ba6c842de563226964a3e2fb5b1a0729b4f2d0d7
sky-bro <sky_bro@qq.com>
2020-01-22 16:14:26 +0800

    0187 Repeated DNA Sequences: bit manipulation

---
+++ leetcode.com/0187 Repeated DNA Sequences/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int char_to_bit(char c){
+        if(c=='A') return 0;
+        if(c=='C') return 1;
+        if(c=='G') return 2;
+        if(c=='T') return 3;
+        return 0;
+    }
+    
+    vector<string> findRepeatedDnaSequences(string s) {
+        int n= s.size(), mask=0, bitmask=(1<<20)-1;
+        if(n==0) return {};
+        unordered_map<int, int> ht;
+        vector<string> result;
+        
+        for(int i=0; i<10; i++){
+            mask= (mask<<2) | char_to_bit(s[i]);
+        }
+        ht[mask]++;
+        
+        for(int i=10; i<n; i++){
+            mask= ((mask<<2) & bitmask) | char_to_bit(s[i]);
+            if(ht.find(mask)!=ht.end() && ht[mask]==1)
+                result.push_back(s.substr(i-9, 10));
+            ht[mask]++;
+        }
+        
+        
+        return result;
+        
+    }
+};
