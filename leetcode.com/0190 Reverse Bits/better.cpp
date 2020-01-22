dd5204d6b8f81c97fffb3fb889f4ea6b36b275e2
sky-bro <sky_bro@qq.com>
2020-01-22 14:19:45 +0800

    0190 Reverse Bits: better bit manipulation

---
+++ leetcode.com/0190 Reverse Bits/better.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for (int i = 0; i < 32; i++) {
+            if (n & 1) {
+                res = (res << 1) + 1;
+            } else {
+                res = res << 1;
+            }
+            n = n >> 1;
+        }
+        return res;
+    }
+};
