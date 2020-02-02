933e532fb7be6d1dd3211a7d82975ba1cb4ebf40
sky-bro <sky_bro@qq.com>
2020-02-02 19:13:51 +0800

    0237 Delete Node in a Linked List: silly

---
+++ leetcode.com/0237 Delete Node in a Linked List/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        // ListNode *pnode = node->next;
+        node->next = node->next->next;
+        // delete pnode;
+    }
+};
