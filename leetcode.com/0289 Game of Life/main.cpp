146c379d6fb722525619468be1efd31ebe73c54d
sky-bro <sky_bro@qq.com>
2020-02-04 22:26:21 +0800

    0289 Game of Life: bit manipulation

--- leetcode.com/0289 Game of Life/main.cpp
+++ leetcode.com/0289 Game of Life/main.cpp
@@ -5,45 +5,23 @@
 using namespace std;
 
 class Solution {
-private:
-    int m, n;
-    inline int getLiveNeighborCnt(int i, int j, vector<vector<int>> &board) {
-        int cnt = - board[i][j];
-        int up_bound = i - 1 >= 0 ? i - 1 : i;
-        int down_bound = i + 1 < m ? i + 1 : i;
-        int left_bound = j - 1 >= 0 ? j - 1 : j;
-        int right_bound = j + 1 < n ? j + 1 : j;
-
-        for (int r = up_bound; r <= down_bound; ++r) {
-            for (int c = left_bound; c <= right_bound; ++c) {
-                cnt += board[r][c];
-            }
-        }
-
-        return cnt;
-
-    }
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        m = board.size(); if (m == 0) return;
-        n = board[0].size(); if (n == 0) return;
-        vector<pair<int, int>> live;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int live_neighbor_cnt = getLiveNeighborCnt(i, j, board);
-                if (live_neighbor_cnt == 3 || live_neighbor_cnt == 2 && board[i][j]) {
-                    live.push_back(pair(i, j));
+        int height = board.size(),width = height?board[0].size():0;
+        for(int i=0;i<height;i++){
+            for(int j = 0;j< width;j++){
+                int cnt = 0;
+                for(int ii = max(0,i-1);ii<min(i+2,height);ii++){
+                    for(int jj = max(0,j-1);jj<min(j+2,width);jj++){
+                        cnt+=board[ii][jj] & 1;
+                    }
                 }
+                if(cnt==3||cnt-board[i][j]==3)
+                    board[i][j] |=2;
             }
         }
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                board[i][j] = 0;
-            }
-        }
-        for (auto &p: live) {
-            board[p.first][p.second] = 1;
-        }
-
+        for(int i = 0;i<height;i++)
+            for(int j =0;j<width;j++)
+                board[i][j] >>= 1;
     }
 };
