c4e2d7284e992dad9d4eda7d6f8210258694e5cb
sky-bro <sky_bro@qq.com>
2020-02-06 17:35:41 +0800

    0303 Range Sum Query - Immutable: better, accumulate

--- leetcode.com/0303 Range Sum Query - Immutable/main.cpp
+++ leetcode.com/0303 Range Sum Query - Immutable/main.cpp
@@ -3,24 +3,19 @@
 
 using namespace std;
 
+// ref: https://leetcode.com/problems/range-sum-query-immutable/discuss/500329/C%2B%2B-Logical-DP-Solution
 class NumArray {
 private:
-    int n;
-    vector<vector<int>> dp;
+    vector<int> accumulate{0};
 public:
     NumArray(vector<int>& nums) {
-        n = nums.size();
-        dp.resize(n, vector<int>(n, 0));
-        for (int i = 0; i < n; ++i) dp[0][i] = nums[i];
-        for (int len = 1; len < n; ++len) {
-            for (int start = 0; start + len < n; ++start) {
-                dp[len][start] = dp[len-1][start] + nums[start+len];
-            }
+        for (int num: nums) {
+            accumulate.push_back(num+accumulate.back());
         }
     }
     
     int sumRange(int i, int j) {
-        return dp[j-i][i];
+        return accumulate[j+1] - accumulate[i];
     }
 };
 
