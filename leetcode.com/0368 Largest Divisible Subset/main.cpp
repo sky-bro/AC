b3a6a32bf7554ed0f7fcd0fcf1cfab8e9408cfea
sky-bro <sky_bro@qq.com>
2020-02-18 09:36:20 +0800

    0368 Largest Divisible Subset: dp

--- leetcode.com/0368 Largest Divisible Subset/main.cpp
+++ leetcode.com/0368 Largest Divisible Subset/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <set>
 #include <vector>
@@ -7,22 +8,39 @@ using namespace std;
 class Solution {
  public:
   vector<int> largestDivisibleSubset(vector<int>& nums) {
+    sort(nums.rbegin(), nums.rend());
     vector<int> res;
-    multiset<int> num_set(nums.begin(), nums.end());
-    while (num_set.size() <= res.size()) {
-      vector<int> tmp_res;
-      auto it = num_set.begin();
-      tmp_res.push_back(*it);
-      num_set.erase(it++);
-      for (; it != num_set.end();) {
-        if ((*it) % tmp_res.back() == 0) {
-          tmp_res.push_back(*it);
-          num_set.erase(it++);
-        } else {
-          ++it;
+    int n = nums.size();
+    if (n == 0) return res;
+
+    vector<int> next(n, -1);
+    vector<int> len(n, 1);
+    int longest_start = 0, max_len = 1;
+
+    for (int i = 1; i < n; ++i) {
+      for (int j = 0; j < i; ++j) {
+        if (len[i] < len[j] + 1 && nums[j] % nums[i] == 0) {
+          len[i] = len[j] + 1;
+          next[i] = j;
         }
       }
-      if () }
+      if (len[i] > max_len) {
+        longest_start = i;
+        max_len = len[i];
+      }
+    }
+    while (longest_start != -1) {
+      res.push_back(nums[longest_start]);
+      longest_start = next[longest_start];
+    }
     return res;
   }
 };
+
+int main(int argc, char const* argv[]) {
+  Solution s;
+  vector<int> nums = {1, 2, 3};
+  nums = {1, 2, 3, 8, 8, 6, 4, 10};
+  s.largestDivisibleSubset(nums);
+  return 0;
+}
