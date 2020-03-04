e433735f0e24267274bb7fe413344b4f9543d06d
sky-bro <sky_bro@qq.com>
2020-03-04 23:59:57 +0800

    0436 Find Right Interval: binary search

---
+++ leetcode.com/0436 Find Right Interval/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<int> findRightInterval(vector<vector<int>>& intervals) {
+    map<int, int> start2index;
+    int n = intervals.size();
+    for (int i = 0; i < n; ++i) {
+      start2index[intervals[i][0]] = i;
+    }
+    vector<int> res;
+    for (int i = 0; i < n; ++i) {
+      auto it =
+          lower_bound(start2index.begin(), start2index.end(), intervals[i][1]);
+      if (it == start2index.end())
+        res.push_back(-1);
+      else
+        res.push_back(it->second);
+    }
+    return res;
+  }
+};
