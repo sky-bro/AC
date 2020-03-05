1984008a1fc0d21963b69045cd0e22c0c5896256
sky-bro <sky_bro@qq.com>
2020-03-05 21:22:01 +0800

    0443 String Compression: add comments

--- leetcode.com/0443 String Compression/main.c
+++ leetcode.com/0443 String Compression/main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
+// sprintf:
+// bug 1: sprintf will add '\0' to terminate string (fixed)
+// bug 2: for chars = ['a', 'a'], sprintf will cause overflow by 1 char:
+// chars[2] = '\0' (fixed)
 int compress(char* chars, int n) {
   int res = 0;
   for (int i = 0; i < n;) {
     int cnt = 1;
+    // 1. write character
     chars[res++] = chars[i++];
     for (; i < n && chars[i] == chars[i - 1]; ++i) ++cnt;
-    if (cnt != 1) {
-      char saved = 0;
-      if (res == i - 1) {
-        chars[res++] = '2';
-      } else {
-        // bug 1: sprintf will add '\0' to terminate string (fixed)
-        // bug 2: for chars = ['a', 'a'], sprintf will cause overflow by 1 char:
-        // chars[2] = '\0' (fixed)
-        res += sprintf(chars + res, "%d", cnt);
-      }
-    }
+    if (cnt == 1) continue;  // single character, no cnt
+    // 2. write cnt
+    if (res == i - 1)
+      chars[res++] = '2';  // chars[res] must be '2'
+    else
+      res += sprintf(chars + res, "%d", cnt);  // won't affect chars[i..]
   }
   return res;
 }
