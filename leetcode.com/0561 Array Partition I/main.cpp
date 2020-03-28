2efac3a24a878f8e7747d93ac3168b663b0da91b
sky-bro <sky_bro@qq.com>
2020-03-28 18:54:03 +0800

    0561 Array Partition I: nlogn sort

---
+++ leetcode.com/0561 Array Partition I/main.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int arrayPairSum(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int res = 0;
+    for (int i = 0; i < nums.size(); i += 2) {
+      res += nums[i];
+    }
+    return res;
+  }
+};
