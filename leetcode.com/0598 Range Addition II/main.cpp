84a6e52d133d544391e093c8d4be36d8671a8fb4
sky-bro <sky_bro@qq.com>
2020-04-02 00:04:07 +0800

    0598 Range Addition II

---
+++ leetcode.com/0598 Range Addition II/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maxCount(int m, int n, vector<vector<int>>& ops) {
+    for (auto& op : ops) {
+      m = min(m, op[0]);
+      n = min(n, op[1]);
+    }
+    return m * n;
+  }
+};
