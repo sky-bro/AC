9b3eb9a927f7bc5c23ee1f2a881ebf2051f08a30
sky-bro <sky_bro@qq.com>
2020-04-09 10:18:31 +0800

    update include

--- leetcode.com/include/leetcode.hpp
+++ leetcode.com/include/leetcode.hpp
@@ -2,8 +2,29 @@
 #include <iostream>
 #include <queue>
 #include <sstream>
+#include <tuple>
 
 using namespace std;
+
+// ###################
+// ##### PRINTER #####
+// ###################
+
+// print array
+template <typename T>
+void printArr(const vector<T> &arr) {
+  for (const T &t : arr) cout << t << " ";
+  cout << endl;
+}
+
+template <typename T>
+void printArr2D(const vector<vector<T>> &arr2D) {
+  for (const auto &vt : arr2D) printArr(vt);
+}
+
+// ###################
+// #### Structure ####
+// ###################
 
 struct TreeNode {
   int val;
@@ -18,6 +39,59 @@ struct ListNode {
   ListNode(int x) : val(x), next(NULL) {}
 };
 
+// ###################
+// ###### Hash #######
+// ###################
+
+// hash tuple
+// from:
+// https://stackoverflow.com/questions/7110301/generic-hash-for-tuples-in-unordered-map-unordered-set#7115547
+namespace std {
+
+namespace {
+// Code from boost
+// Reciprocal of the golden ratio helps spread entropy
+//     and handles duplicates.
+// See Mike Seymour in magic-numbers-in-boosthash-combine:
+//     https://stackoverflow.com/questions/4948780
+
+template <class T>
+inline void hash_combine(std::size_t &seed, T const &v) {
+  seed ^= hash<T>()(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+// Recursive template code derived from Matthieu M.
+template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
+struct HashValueImpl {
+  static void apply(size_t &seed, Tuple const &tuple) {
+    HashValueImpl<Tuple, Index - 1>::apply(seed, tuple);
+    hash_combine(seed, std::get<Index>(tuple));
+  }
+};
+
+template <class Tuple>
+struct HashValueImpl<Tuple, 0> {
+  static void apply(size_t &seed, Tuple const &tuple) {
+    hash_combine(seed, std::get<0>(tuple));
+  }
+};
+}  // namespace
+
+template <typename... TT>
+struct hash<std::tuple<TT...>> {
+  size_t operator()(std::tuple<TT...> const &tt) const {
+    size_t seed = 0;
+    HashValueImpl<std::tuple<TT...>>::apply(seed, tt);
+    return seed;
+  }
+};
+
+}  // namespace std
+
+// ###################
+// ##### Parser ######
+// ###################
+
 void trimLeftTrailingSpaces(string &input) {
   input.erase(input.begin(), find_if(input.begin(), input.end(),
                                      [](int ch) { return !isspace(ch); }));
@@ -30,6 +104,7 @@ void trimRightTrailingSpaces(string &input) {
       input.end());
 }
 
+// Parse tree
 TreeNode *stringToTreeNode(string input) {
   trimLeftTrailingSpaces(input);
   trimRightTrailingSpaces(input);
@@ -74,4 +149,15 @@ TreeNode *stringToTreeNode(string input) {
     }
   }
   return root;
+}
+
+// ###################
+// #### Destroyer ####
+// ###################
+
+void delete_tree(TreeNode *root) {
+  if (!root) return;
+  delete_tree(root->left);
+  delete_tree(root->right);
+  delete root;
 }
