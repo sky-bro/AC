9188cf1b8008a9538485f5db2faf311c4e849e09
sky-bro <sky_bro@qq.com>
2020-04-10 12:19:50 +0800

    parse vector

--- leetcode.com/include/leetcode.hpp
+++ leetcode.com/include/leetcode.hpp
@@ -88,9 +88,9 @@ struct hash<std::tuple<TT...>> {
 
 }  // namespace std
 
-// ###################
-// ##### Parser ######
-// ###################
+// #############################
+// ##### Parser/Converter ######
+// #############################
 
 void trimLeftTrailingSpaces(string &input) {
   input.erase(input.begin(), find_if(input.begin(), input.end(),
@@ -149,6 +149,27 @@ TreeNode *stringToTreeNode(string input) {
     }
   }
   return root;
+}
+
+// parse integer vector
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+// bool to string
+string boolToString(bool input) {
+    return input ? "True" : "False";
 }
 
 // ###################
