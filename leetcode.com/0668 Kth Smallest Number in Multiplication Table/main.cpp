9acbd41f4dcc0723c1cf5814ef76f28d489d2a6d
sky-bro <sky_bro@qq.com>
2020-04-10 17:23:44 +0800

    0668 Kth Smallest Number in Multiplication Table: binary search

---
+++ leetcode.com/0668 Kth Smallest Number in Multiplication Table/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// similar problem: 378. Kth Smallest Element in a Sorted Matrix
+// https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int l = 1, r = m *n;
+        while (l < r) {
+          int mid = (l + r) / 2;
+          int j = n, cnt = 0;
+          for (int i = 1; i <= m; ++i) {
+            while (i*j > mid) --j;
+            cnt += j;
+          }
+          if (cnt < k) l = mid + 1;
+          else r = mid;
+        }
+        return l;
+    }
+};
