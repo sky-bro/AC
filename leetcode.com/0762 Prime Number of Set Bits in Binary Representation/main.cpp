6a8e5901dab3cd43c6f04587b4f4f5afe3cb50ec
sky-bro <sky_bro@qq.com>
2020-05-04 17:42:55 +0800

    0762 Prime Number of Set Bits in Binary Representation: trick, bitmanipulation

--- leetcode.com/0762 Prime Number of Set Bits in Binary Representation/main.cpp
+++ leetcode.com/0762 Prime Number of Set Bits in Binary Representation/main.cpp
@@ -3,13 +3,14 @@
 
 using namespace std;
 
-// static vector<bool> a = [](){
-//   vector<bool> res(33, true);
-//   for (int i = 2; i < 33; ++i) {
+// ref: 665772
+// https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/discuss/113232/665772
 
-//   }
-  
-// }();
+static vector<bool> a = [](){
+  vector<bool> res(33);
+  res[2] = res[3] = res[5] = res[7] = res[11] = res[13] = res[17] = res[19] = res[23] = res[29] = res[31] = true;
+  return res;
+}();
 
 class Solution {
 private:
@@ -25,8 +26,10 @@ public:
     int countPrimeSetBits(int L, int R) {
         int res = 0;
         while (L <= R) {
-          if (chk(L)) ++res;
-          ++L;
+          // if (chk(L)) ++res;
+          // if (a[__builtin_popcount(L)]) ++res;
+          res += 665772 >> __builtin_popcount(L++) & 1;
+          // ++L;
         }
         return res;
     }
