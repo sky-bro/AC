e7ed40b051291ef82e32879b3e16899528871b7b
sky-bro <sky_bro@qq.com>
2020-09-12 10:56:42 +0800

    0814 Binary Tree Pruning: recursive

---
+++ leetcode.com/0814 Binary Tree Pruning/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  TreeNode *pruneTree(TreeNode *root) {
+    if (!root) return;
+    root->left = pruneTree(root->left);
+    root->right = pruneTree(root->right);
+    if (!(root->left || root->right) && !root->val) {
+      // delete root;  // ?
+      return nullptr;
+    }
+    return root;
+  }
+};
