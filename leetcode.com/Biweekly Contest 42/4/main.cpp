2f60a2830b6ce64929c246d2bb00063cb3b817b5
Ky13 <sky_io@outlook.com>
2020-12-27 17:15:01 +0800

    median

--- leetcode.com/Biweekly Contest 42/4/main.cpp
+++ leetcode.com/Biweekly Contest 42/4/main.cpp
@@ -10,26 +10,21 @@ static int x = []() {
 typedef long long ll;
 
 class Solution {
- public:
-  int minMoves(vector<int>& nums, int k) {
-    if (k == 1) return 0;
+public:
+  int minMoves(vector<int> &nums, int k) {
+    if (k == 1)
+      return 0;
     int n = 0;
-    vector<ll> pos;
+    vector<ll> pos, sums;
     for (int i = 0; i < nums.size(); ++i) {
       if (nums[i]) {
-        pos.push_back(i - (n++));
+        pos.push_back(i - n);
+        sums.push_back(pos[n++] + (sums.empty() ? 0 : sums.back()));
       }
-    }
-    vector<ll> sums(n);
-    sums[0] = pos[0];
-    for (int i = 1; i < n; ++i) {
-      sums[i] = pos[i] + sums[i - 1];
     }
     ll res = INT32_MAX;
     int l = k / 2, r = k - l - 1;
-    for (int i = 0; i + k <= n; ++i) {
-      int m = i + k / 2;
-      // int l = m-i, r = i+k-m;
+    for (int i = 0, m = k / 2; i + k <= n; ++i, ++m) {
       ll cur = pos[m] * l - (sums[m - 1] - sums[i] + pos[i]) - pos[m] * r +
                sums[i + k - 1] - sums[m];
       res = min(cur, res);
@@ -38,7 +33,7 @@ class Solution {
   }
 };
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[]) {
   Solution s;
   vector<int> nums = {1, 0, 0, 1, 0, 1};
   int k = 2;
