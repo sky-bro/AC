b16784ef20f5da62e1b83d7a0e22e905f75ea62f
Ky13 <sky_io@outlook.com>
2021-02-22 17:45:14 +0800

    change to template

--- Algorithms/BIT/main.cpp
+++ Algorithms/BIT/main.cpp
@@ -3,40 +3,44 @@
 
 using namespace std;
 
-int n;
-vector<int> A;
-
 template <typename T>
 void printArr(const vector<T> &arr) {
   for (const T &t : arr) cout << t << " ";
   cout << endl;
 }
 
-int sum(int i) {  // A[1] + A[2] + ... + A[i]
-  int res = 0;
-  while (i) res += A[i], i -= i & -i;
-  return res;
-}
-
-void add(int i, int k) {  // adds k to A[i]
-  while (i <= n) A[i] += k, i += i & -i;
-}
+template <typename T>
+class fenwick {
+public:
+  int n;
+  vector<T> A;
+  fenwick(int n): n(n), A(n+1) {} // A[0] not used
+  T sum(int i) {  // prefix sum: A[1] + A[2] + ... + A[i]
+    T v{};
+    while (i) v += A[i], i -= i & -i;
+    return v;
+  }
+  void add(int i, T v) {  // adds v to A[i]
+    while (i <= n) A[i] += v, i += i & -i;
+  }
+};
 
 int main(int argc, char const *argv[]) {
   cout << "input the number of elements to be added, n: ";
+  int n;
   cin >> n;
-  A.resize(n + 1);
+  fenwick<int> t(n); // 1-indexed
   for (int i = 1; i <= n; ++i) {
-    cin >> A[0];
-    add(i, A[0]);
+    cin >> t.A[0];
+    t.add(i, t.A[0]);
   }
-//   printArr(A);
+  printArr(t.A);
   while (1) {
     cout << "get sum of first x elements, x: ";
     int x;
     cin >> x;
     if (x > 0 && x <= n)
-      cout << "sum of first " << x << " elements is " << sum(x) << endl;
+      cout << "sum of first " << x << " elements is " << t.sum(x) << endl;
     else
       break;
   }
