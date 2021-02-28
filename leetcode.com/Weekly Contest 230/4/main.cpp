8cbf9a675f38300ac186923493d2805f1df3b166
Ky13 <sky_io@outlook.com>
2021-03-01 00:17:40 +0800

    add pp tuple

--- leetcode.com/Weekly Contest 230/4/main.cpp
+++ leetcode.com/Weekly Contest 230/4/main.cpp
@@ -2,13 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int __fastio = []() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; } ();
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+// template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p); // do not need?
+// template<typename...Ts> ostream& operator<<(ostream &os, const tuple<Ts...> &t);
 template<typename A> ostream& operator<<(ostream &os, const set<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
 template<typename A> ostream& operator<<(ostream &os, const multiset<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e.first << ": " << e.second, sep = ", "; return os << "}"; }
 template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "]"; }
 template<typename T, size_t L>ostream& operator<<(ostream& os, const array<T, L>& v) { os << "["; string sep = ""; for (int i = 0; i < L; ++i) os << sep << v[i], sep = ", "; return os << "]"; }
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+template<typename...Ts> ostream& operator<<(ostream &os, const tuple<Ts...> &t) { os << '('; apply([&os](Ts const&...args) { size_t i = 0, n = sizeof...(Ts); ((os << args << (++i != n ? ", " : "")), ...); }, t); return os << ')'; }
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #ifdef SKY_DEBUG
@@ -66,14 +68,20 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    Solution s;
-    vector<vector<int>> A = {
-        {1,2},{2,1},{4,3},{7,2}
-    };
-    A = {
-        {3,4},{5,4},{6,3},{9,1}
-    };
-    pp(s.getCollisionTimes(A));
+    // Solution s;
+    // vector<vector<int>> A = {
+    //     {1,2},{2,1},{4,3},{7,2}
+    // };
+    // A = {
+    //     {3,4},{5,4},{6,3},{9,1}
+    // };
+    // pp(s.getCollisionTimes(A));
+    auto a = make_tuple(5, "hello", 1.3, false, make_pair<int, char>(12, 'x'));
+    pp(a);
+    // map<int, vector<int>> mp;
+    // vector<int> vi = {1,2,3};
+    // mp[0] = vi;
+    // pp(mp);
     return 0;
 }
 
