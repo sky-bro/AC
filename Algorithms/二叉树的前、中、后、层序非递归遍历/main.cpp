94b293bb76180160073fe0c09d46e513bac400e2
Ky13 <sky_io@outlook.com>
2021-09-06 10:30:31 +0800

    postorder, levelorder

--- Algorithms/二叉树的前、中、后、层序非递归遍历/main.cpp
+++ Algorithms/二叉树的前、中、后、层序非递归遍历/main.cpp
@@ -3,6 +3,7 @@
 // https://blog.csdn.net/qq_35812205/article/details/96476519
 #include <iostream>
 #include <stack>
+#include <queue>
 #include <vector>
 
 using namespace std;
@@ -14,10 +15,11 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// leetcode 144. Binary Tree Preorder Traversal
 vector<int> preorder_traversal(TreeNode* root) {
     vector<int> result;
     stack<TreeNode*> stk;
-    stk.push(root);
+    if (root) stk.push(root);
     while (!stk.empty()) {
         TreeNode *pnode = stk.top(); stk.pop();
         result.push_back(pnode->val);
@@ -28,6 +30,7 @@ vector<int> preorder_traversal(TreeNode* root) {
     return result;
 }
 
+// leetcode 94. Binary Tree Inorder Traversal
 vector<int> inorder_traversal(TreeNode* root) {
     vector<int> result;
     stack<TreeNode*> stk;
@@ -44,15 +47,47 @@ vector<int> inorder_traversal(TreeNode* root) {
     return result;
 }
 
+// leetcode 145. Binary Tree Postorder Traversal
 vector<int> postorder_traversal(TreeNode* root) {
     vector<int> result;
-
+    stack<TreeNode*> stk;
+    TreeNode *pre = nullptr;
+    do {
+        while (root) {
+            stk.push(root);
+            root = root->left;
+        }
+        
+        TreeNode *pre = nullptr;
+        while (!stk.empty()) {
+            root = stk.top();
+            if (root->right == pre) {
+                result.push_back(root->val);
+                stk.pop();
+                pre = root;
+            } else {
+                root = root->right;
+                break;
+            }
+        }
+    } while (!stk.empty());
     return result;
 }
 
-vector<int> levelorder_traversal(TreeNode* root) {
-    vector<int> result;
-
+vector<vector<int>> levelorder_traversal(TreeNode* root) {
+    vector<vector<int>> result;
+    queue<TreeNode*> q;
+    if (root) q.push(root);
+    while (!q.empty()) {
+        vector<int> row;
+        for (int sz = q.size(); sz; --sz) {
+            TreeNode *p = q.front(); q.pop();
+            row.push_back(p->val);
+            if (p->left) q.push(p->left);
+            if (p->right) q.push(p->right);
+        }
+        result.push_back(row);
+    }
     return result;
 }
 
@@ -65,12 +100,14 @@ void print_arr(const vector<int> &vals) {
 
 int main(int argc, char const *argv[])
 {
-    TreeNode root(1), left(0), right(2);
-    root.left = &left; root.right = &right;
+    TreeNode root(1), left(2), right(3);
+    // root.left = &left; root.right = &right;
+    root.right = &left;
+    left.left = &right;
 
     print_arr(preorder_traversal(&root));
     print_arr(inorder_traversal(&root));
-    // print_arr(postorder_traversal(&root));
+    print_arr(postorder_traversal(&root));
     // print_arr(levelorder_traversal(&root));
 
     return 0;
